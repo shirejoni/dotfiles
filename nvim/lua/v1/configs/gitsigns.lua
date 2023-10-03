e104bcc72e2406d6ec089cd072395cd262a8e694
shirejoni <hosseinshirjoni@chmail.ir>
2023-10-03 16:06:18 +0330

    nvim setup

--- nvim/lua/v1/configs/gitsigns.lua
+++ nvim/lua/v1/configs/gitsigns.lua
@@ -17,6 +17,7 @@ return {
 			watch_gitdir = {
 				follow_files = true,
 			},
+			_signs_staged_enable = true,
 			attach_to_untracked = true,
 			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 			current_line_blame_opts = {
@@ -28,8 +29,10 @@ return {
 			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
 			sign_priority = 6,
 			update_debounce = 100,
+
 			status_formatter = nil, -- Use default
 			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+
 			preview_config = {
 				-- Options passed to nvim_open_win
 				border = "single",
